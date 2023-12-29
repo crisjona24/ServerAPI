fb2795e35ca7b8766a17dc22220849eff81bea18
Cristobal Jonathan Rios Simancas <78310047+crisjona24@users.noreply.github.com>
2023-12-29 17:59:04 +0000

    listo

--- proyecto_WAPIPTDAH/settings.py
+++ proyecto_WAPIPTDAH/settings.py
@@ -214,6 +214,26 @@ EMAIL_USE_SSL = False
 """ CONFIGURACIÓN PARA LOS CORS HEADERS DE ACCESO DEL FRONTEND """
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
 
 CORS_ALLOWED_ORIGINS = [
     "https://react-frontend-production-b574.up.railway.app",
